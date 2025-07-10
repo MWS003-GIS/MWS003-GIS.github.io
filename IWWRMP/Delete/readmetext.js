b09523b497e61db35a27df548372081d333ae179
MWS003-GIS <iwmi.iwwrm.gis@gmail.com>
2025-07-10 15:16:35 +0530

    edit7

--- IWWRMP/Delete/readmetext.js
+++ IWWRMP/Delete/readmetext.js
@@ -13,19 +13,3 @@ window.onload = function() {
         document.getElementById('readme-modal').style.display = 'flex';
     };
 };
-
-window.onload = function() {
-
-    // Show README modal on load
-    document.getElementById('readme-modal').style.display = 'flex';
-
-    // Close README modal
-    document.getElementById('close-readme').onclick = function() {
-        document.getElementById('readme-modal').style.display = 'none';
-    };
-
-    // Show README modal when "Help" button is clicked
-    document.getElementById('help-button').onclick = function() {
-        document.getElementById('readme-modal').style.display = 'flex';
-    };
-};
