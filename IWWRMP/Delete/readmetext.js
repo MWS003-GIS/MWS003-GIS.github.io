6c8cc9c56413c5d40b82bcf2e4d253c2625cc739
MWS003-GIS <iwmi.iwwrm.gis@gmail.com>
2025-07-10 13:36:08 +0530

    delete page

---
+++ IWWRMP/Delete/readmetext.js
@@ -0,0 +1,33 @@
+<<<<<<< HEAD
+window.onload = function() {
+
+    // Show README modal on load
+    document.getElementById('readme-modal').style.display = 'flex';
+
+    // Close README modal
+    document.getElementById('close-readme').onclick = function() {
+        document.getElementById('readme-modal').style.display = 'none';
+    };
+
+    // Show README modal when "Help" button is clicked
+    document.getElementById('help-button').onclick = function() {
+        document.getElementById('readme-modal').style.display = 'flex';
+    };
+};
+=======
+window.onload = function() {
+
+    // Show README modal on load
+    document.getElementById('readme-modal').style.display = 'flex';
+
+    // Close README modal
+    document.getElementById('close-readme').onclick = function() {
+        document.getElementById('readme-modal').style.display = 'none';
+    };
+
+    // Show README modal when "Help" button is clicked
+    document.getElementById('help-button').onclick = function() {
+        document.getElementById('readme-modal').style.display = 'flex';
+    };
+};
+>>>>>>> 5cbfb5af309b66b3e85f2911040423d43e79fda9
---
+++ IWWRMP/Delete/scale.js
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+
+ L.control.scale({
+      position: 'bottomright', // Position of the scale bar on the map
+      imperial: true,          // Show metric scale only
+      metric: true              // Show metric scale only
+    }).addTo(map);
+=======
+
+ L.control.scale({
+      position: 'bottomright', // Position of the scale bar on the map
+      imperial: true,          // Show metric scale only
+      metric: true              // Show metric scale only
+    }).addTo(map);
+>>>>>>> 5cbfb5af309b66b3e85f2911040423d43e79fda9
